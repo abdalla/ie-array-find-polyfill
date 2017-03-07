6df766a4485ec76877a2e61c2caba704a90f72d3
Carlos Abdalla <abdalla@msdev.com.br>
2017-03-07 09:16:34 -0600

    polyfill

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ie-array-find-polyfill
 Polyfill to provide array.find on IE.
+
+# Installation
+To install the stable version:
+
+'''zsh
+npm install ie-array-find-polyfill --save
+'''
+
+# Use
+'''javascript
+import ie-array-find-polyfill
+'''
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+        value: function (predicate) {
+
+            if (this == null) {
+                throw new TypeError('this is null or not defined');
+            }
+
+            var obj = Object(this);
+            var len = obj.length >>> 0;
+
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+
+            var thisArg = arguments[1];
+
+            var index = 0;
+
+            while (index < len) {
+                var iValue = obj[index];
+                if (predicate.call(thisArg, iValue, index, obj)) {
+                    return iValue;
+                }
+                index++;
+            }
+
+            return undefined;
+        }
+    });
+}
